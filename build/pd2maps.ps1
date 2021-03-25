5ac278a76a4665f79b060e055b396d965e710694
TheIrateSeagoer <79901155+TheIrateSeagoer@users.noreply.github.com>
2021-03-25 13:51:10 -0400

    Maps

--- build/pd2maps.ps1
+++ build/pd2maps.ps1
@@ -1,17 +1,3 @@
-Write-Host -NoNewline "Default ... "
-.\pd2default.ps1
-Write-Host "Done"
-
-Write-Host -NoNewline "Maps ... "
-.\pd2maps.ps1
-Write-Host "Done"
-
-Write-Host -NoNewline "Show All ... "
-.\pd2showall.ps1
-Write-Host "Done"
-
-Write-Host -NoNewline "Personal ... "
-
 if (Test-Path -Path '.\obj') { Remove-Item '.\obj' -Recurse }
 if (Test-Path -Path '.\bin') { Remove-Item '.\bin' -Recurse }
 
@@ -25,13 +11,12 @@ ForEach-Object{
     
     Get-Content -Path $inputPath | Where-Object {$_.trim() -ne "" } | Set-Content -Path $outputPath 
     (Get-Content -Path $outputPath | Select-String -pattern '/////' -notmatch) | Set-Content -Path $outputPath
-    (Get-Content -Path $outputPath).replace("`t","").replace("    ]","]").replace(" ]","]").replace(" ]","]")| Set-Content -Path $outputPath 
+    (Get-Content -Path $outputPath).replace("`t","").replace("    ]","]").replace(" ]","]").replace(" ]","]")| Set-Content -Path $outputPath
 }
 
 Get-Content '.\obj\quest.lod.filter', '.\obj\quest.pd2.filter', 
 '.\obj\common.lod.filter', '.\obj\common.pd2.filter', 
 '.\obj\item.prefix.filter', 
-#'.\obj\item.treasureclass.filter', 
 '.\obj\item.crafting.filter', 
 '.\obj\item.suffix.filter', '.\obj\item.shopping.filter', '.\obj\item.colorfix.filter', 
 '.\obj\bases.filter', '.\obj\bases.larzuk.filter', 
@@ -40,9 +25,9 @@ Get-Content '.\obj\quest.lod.filter', '.\obj\quest.pd2.filter',
 '.\obj\arreat.normal.filter', '.\obj\arreat.exceptional.filter', '.\obj\arreat.elite.filter', 
 '.\obj\set.shared.filter', '.\obj\set.normal.filter', '.\obj\set.exceptional.filter', '.\obj\set.elite.filter', 
 '.\obj\unique.shared.filter', '.\obj\unique.normal.filter', '.\obj\unique.exceptional.filter', '.\obj\unique.elite.filter', 
-'.\obj\catchall.default.filter' | Set-Content '.\bin\personal.filter'
+'.\obj\catchall.maps.filter' | Set-Content '.\bin\maps.filter'
 
-if (Test-Path -Path 'C:\Program Files (x86)\Diablo II - PD2\ProjectD2') { Copy-Item '.\bin\personal.filter' -Destination 'C:\Program Files (x86)\Diablo II - PD2\ProjectD2\loot.filter' }
-if (Test-Path -LiteralPath 'C:\Program Files (x86)\Diablo II - Cactus\BH.cfg') { Copy-Item '.\bin\personal.filter' -Destination 'C:\Program Files (x86)\Diablo II - Cactus\BH.cfg' }
+if (Test-Path -Path 'C:\Program Files (x86)\Diablo II - PD2\ProjectD2') { Copy-Item '.\bin\maps.filter' -Destination 'C:\Program Files (x86)\Diablo II - PD2\ProjectD2\loot.filter' }
+if (Test-Path -LiteralPath 'C:\Program Files (x86)\Diablo II - Cactus\BH.cfg') { Copy-Item '.\bin\maps.filter' -Destination 'C:\Program Files (x86)\Diablo II - Cactus\BH.cfg' }
 
-Write-Host "Done"
+Copy-Item '.\bin\maps.filter' -Destination '..\maps.filter'
