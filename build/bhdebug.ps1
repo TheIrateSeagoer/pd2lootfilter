30476ea58d36b4c0ef166ed18e730ddd252aa3c3
TheIrateSeagoer <79901155+TheIrateSeagoer@users.noreply.github.com>
2021-04-06 23:24:23 -0400

    Update bhdebug.ps1

--- build/bhdebug.ps1
+++ build/bhdebug.ps1
@@ -1,2 +1,2 @@
-[System.Environment]::SetEnvironmentVariable('DIABLO_DEBUG_FOLDER','C:\Program Files (x86)\Diablo II - PD2 - Debug',[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('DIABLO_DEBUG_FOLDER','C:\Program Files (x86)\Diablo II - PD2 - Debug\ProjectD2',[System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable('DIABLO_DEBUG_COMMAND_LINE_ARGUMENTS','-w -3dfx -sndbkg',[System.EnvironmentVariableTarget]::Machine)
